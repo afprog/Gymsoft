ca60d62754c7f63ec4265cdf8b5b92858bc67c0b
Afprog <aalzate86@gmail.com>
2013-09-13 18:08:00 -0500

    Cambios en Requerimiento

--- GymsoftJPA/src/co/edu/uniminuto/gymsoft/persistence/model/Usuario.java
+++ GymsoftJPA/src/co/edu/uniminuto/gymsoft/persistence/model/Usuario.java
@@ -16,7 +16,7 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private int idUsuario;
 
 	private int altura;
